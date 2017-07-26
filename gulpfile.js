1c2412834409e61f6864337ed3a6e4b629b2a49f
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2017-07-26 17:46:17 +0100

    Final touches to the structure with Omar

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ let sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default',() => {
     // CSS
-    gulp.src('./src/css/*.css')
+    gulp.src('./public/css/*.css')
         .pipe(sourcemaps.init())
         .pipe(cleanCSS({level: {1: {specialComments: 0}}}))
         .pipe(concat('awin.css'))
         .pipe(sourcemaps.write('.', {includeContent: true}))
         .pipe(gulp.dest('./public/dist/'));
     // JS
-    gulp.src('./src/js/*.js')
+    gulp.src('./public/js/*.js')
        .pipe(concat('awin.js'))
        .pipe(gulp.dest('./public/dist'));
 });
