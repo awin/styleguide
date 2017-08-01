1b3a34f1c300aea33ef19c4f346bdf9aa65f252b
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2017-08-01 16:31:37 +0100

    Clean CSS breaks fontAwesome ...

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,9 @@ gulp.task('default',() => {
     // CSS
     gulp.src('./public/css/*.css')
         .pipe(sourcemaps.init())
-        .pipe(cleanCSS({level: {1: {specialComments: 0}}}))
+        .pipe(cleanCSS({level: { 1: {
+            tidySelectors: false // controls selectors optimizing; defaults to `true`,
+        }}}))
         .pipe(concat('awin.css'))
         .pipe(sourcemaps.write('.', {includeContent: true}))
         .pipe(gulp.dest('./public/dist/'));
