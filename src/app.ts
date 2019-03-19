159ef1eccde70e3154b4a18b13397bb3d5ca407c
Omar <omar.malik@affiliatewindow.com>
2019-03-19 17:06:08 +0000

    UX-2850 adding font-awesome 4 and other paths

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
-import '../node_modules/@fortawesome/fontawesome-pro-webfonts/css/fontawesome.css';
-import '../node_modules/@fortawesome/fontawesome-pro-webfonts/css/fa-solid.css';
-import '../node_modules/@fortawesome/fontawesome-pro-webfonts/css/fa-regular.css';
-import '../node_modules/@fortawesome/fontawesome-pro-webfonts/css/fa-light.css';
-import '../node_modules/@fortawesome/fontawesome-pro-webfonts/css/fa-brands.css';
+import '../node_modules/@fortawesome/fontawesome-pro/css/all.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './css/awinTheme.css';
 import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import './js/styleguide.js';
