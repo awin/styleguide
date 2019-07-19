be0624fe995bfd5cdfb37a726d43191dfe53e98a
Omar <omar.malik@affiliatewindow.com>
2019-07-19 10:51:14 +0100

    UX-3224 removing font awesome free version and updating reference on styleguide

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import '../node_modules/@fortawesome/fontawesome-pro/css/all.css';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './css/awinTheme.css';
 import '../node_modules/jquery/dist/jquery.js';
