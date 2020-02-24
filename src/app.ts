8a0d3fa2a5a1acf15f13e89fde0eecd93457cddf
OmarMalik-awin <omar.malik@awin.com>
2020-02-24 13:28:02 +0000

    UX-3413 crafting private network theme

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ import '../node_modules/@fortawesome/fontawesome-pro/scss/regular.scss';
 import '../node_modules/@fortawesome/fontawesome-pro/scss/light.scss';
 import '../node_modules/font-awesome/scss/font-awesome.scss';
 
-import './css/newAwinTheme.scss';
+import './css/master.scss';
 import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
