e5c82aa6b405105e11d84a49996a342b51944b4a
OmarMalik-awin <omar.malik@awin.com>
2020-11-12 08:39:19 +0000

    UX-3669 Adding nikevip theme

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const themes = ['awin', 'nikepartners','pntest','kerstintest','etsypn','noreportingtest'];
+const themes = ['awin','etsypn','nikepartners','nikevip','pntest','kerstintest','noreportingtest'];
 
 module.exports = themes.map(theme => ({
   name: theme,
